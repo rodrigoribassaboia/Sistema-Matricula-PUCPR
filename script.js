9c8fb094b8bd2506c31d685bec1814f784f2f7e1
Rodrigo Ribas <rodrigoribas99@gmail.com>
2026-06-25 17:25:24 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('form').addEventListener('submit',e=>{
-e.preventDefault();
-document.getElementById('msg').classList.remove('d-none');
-e.target.reset();
-window.scrollTo({top:document.body.scrollHeight,behavior:'smooth'});
-});
